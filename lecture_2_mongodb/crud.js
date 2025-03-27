342a1b930e3c203b38415a3ee32251b615211ab7
Your Name <vinny.purgato@gmail.com>
2025-03-27 19:49:57 -0400

    jsdoc for clarity

--- lecture_2_mongodb/crud.js
+++ lecture_2_mongodb/crud.js
@@ -1,7 +1,11 @@
 // Basic CRUD Operations (Create, Read, Update, Delete)
 
-// CREATE - Insert a new document into MongoDB
-// This function takes a collection and data, then adds the data as a new document
+/**
+ * Creates a new movie document in the MongoDB collection
+ * @param {Collection} movies - The MongoDB collection for movies
+ * @param {Object} movieData - The movie data to insert
+ * @returns {Promise<InsertOneResult>} Result of the insert operation
+ */
 export async function createMovie(movies, movieData) {
   // insertOne() adds a single document to the collection
   const insertResult = await movies.insertOne(movieData);
@@ -9,16 +13,25 @@ export async function createMovie(movies, movieData) {
   return insertResult;
 }
 
-// READ - Retrieve documents from MongoDB
-// This function demonstrates different ways to query documents
+/**
+ * Retrieves a single movie document that matches the query
+ * @param {Collection} movies - The MongoDB collection for movies
+ * @param {Object} query - The query criteria to find the movie
+ * @returns {Promise<Object|null>} The found movie document or null if not found
+ */
 export async function readMovies(movies, query) {
   // Find a single document by exact match
   const movieFound = await movies.findOne(query);
   return movieFound;
 }
 
-// UPDATE - Modify existing documents
-// This function updates documents that match a query
+/**
+ * Updates a single movie document that matches the query
+ * @param {Collection} movies - The MongoDB collection for movies
+ * @param {Object} query - The query criteria to find the movie to update
+ * @param {Object} update - The update operations to perform
+ * @returns {Promise<UpdateResult>} Result of the update operation
+ */
 export async function updateMovie(movies, query, update) {
   // updateOne() modifies a single document that matches the query
   const updateResult = await movies.updateOne(query, update);
@@ -26,8 +39,12 @@ export async function updateMovie(movies, query, update) {
   return updateResult;
 }
 
-// DELETE - Remove documents
-// This function removes documents that match a query
+/**
+ * Deletes a single movie document that matches the query
+ * @param {Collection} movies - The MongoDB collection for movies
+ * @param {Object} query - The query criteria to find the movie to delete
+ * @returns {Promise<DeleteResult>} Result of the delete operation
+ */
 export async function deleteMovie(movies, query) {
   // deleteOne() removes a single document that matches the query
   const deleteResult = await movies.deleteOne(query);
@@ -37,6 +54,11 @@ export async function deleteMovie(movies, query) {
 
 // Advanced Feature: Aggregation Pipeline
 // This demonstrates how to process and analyze data
+/**
+ * Gets statistics about comedy movies using MongoDB's aggregation pipeline
+ * @param {Collection} movies - The MongoDB collection for movies
+ * @returns {Promise<void>}
+ */
 export async function getComedyMovieStats(movies) {
   // Pipeline is an array of stages that process documents
   const pipeline = [
@@ -56,7 +78,12 @@ export async function getComedyMovieStats(movies) {
   }
 }
 
-// GET ALL - Retrieve all movies from the collection
+/**
+ * Retrieves all movies from the collection
+ * @param {Collection} movies - The MongoDB collection for movies
+ * @returns {Promise<Array>} Array of all movie documents
+ * @throws {Error} If there's an error fetching the movies
+ */
 export async function getAllMovies(movies) {
   try {
     // Find all documents in the collection
